54b4cafe2ec0dc538aaab84769d4a7c33b0c7f97
Ajaykumar496 <ajaykumar.lf88@gmail.com>
2025-07-29 16:13:28 -0400

    Initial commit: added sample2.py

---
+++ sample2.py
@@ -0,0 +1,15 @@
+#fstrings
+name = "Arjunreddy"
+age = 24
+print(f"My name is {name} and i am {age} years old")
+#String Indexing 
+print(name[0])
+#String Slicing
+print(name[0:5:1])
+#Conditional Statements
+if age> 18:
+    print("You can vote")
+elif age== 18:
+    print("You can vote")
+else:
+    print("Better Luck next time")
